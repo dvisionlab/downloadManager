211e25a250d7f5f9ec70ce000025c2fccb35edc8
mattia-wsl <mat.ronzoni@gmail.com>
2023-10-18 13:33:59 +0200

    update docs

--- src/index.ts
+++ src/index.ts
@@ -96,6 +96,7 @@ export class DownloadManager {
     // TODO should we check that reworking is ongoing (freeze = true) ?
     // TODO check that key is in the download queue
     this._activeKey = key;
+    this._activeIndex = null;
     this.reworkQueue();
   }
 
@@ -104,6 +105,10 @@ export class DownloadManager {
   }
 
   set activeIndex(index: number | null) {
+    if (!this._activeKey) {
+      console.warn("activeKey is not set");
+      return;
+    }
     this._activeIndex = index;
     this.reworkQueue();
   }
@@ -131,6 +136,17 @@ export class DownloadManager {
   }
 
   /**
+   * Update the qs indexes
+   * @param slotDimension The number of images in the slot
+   */
+  private updateQs(slotDimension: number) {
+    this.qs[0] =
+      this.qs[0] - slotDimension > 0 ? this.qs[0] - slotDimension : 0;
+    this.qs[1] =
+      this.qs[1] - slotDimension > 0 ? this.qs[1] - slotDimension : 0;
+  }
+
+  /**
    * Add a new series in the download manager
    * @returns True if the series was added, false otherwise
    */
@@ -142,7 +158,7 @@ export class DownloadManager {
   ) {
     // check that the series is not already in the seriesData
     // TODO what if I want to add other slices for a series ? we could use s Set for the imagesIds
-    if (seriesId in this.seriesData) {
+    if (key in this.seriesData) {
       console.warn(`Series ${seriesId} is already in the download manager`);
       return false;
     }
@@ -211,7 +227,10 @@ export class DownloadManager {
     this.addingQueue = [];
     this.removingQueue = [];
 
-    if (this.verbose) console.log("downloadQueue", this.downloadQueue);
+    if (this.verbose) {
+      console.log("downloadQueue");
+      console.table(this.downloadQueue);
+    }
 
     // unblock requests
     this.freeze = false;
@@ -252,8 +271,14 @@ export class DownloadManager {
       return null;
     }
     const nextSlot = this.downloadQueue.splice(0, slotDimension);
+    this.updateQs(slotDimension);
     this.updateIsDownloading(nextSlot);
-    if (this.verbose) console.log("nextSlot", nextSlot);
+    if (this.verbose) {
+      console.log("nextSlot");
+      console.table(nextSlot);
+      console.log("downloadQueue");
+      console.table(this.downloadQueue);
+    }
     return nextSlot;
   }
 
